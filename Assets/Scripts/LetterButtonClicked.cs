4102dbdcb6fa6d419ac0560ede6be4c36606a4f2
1062993 <s1062993@mail.ncyu.edu.tw>
2022-03-01 21:18:11 +0800

    Add original project to main

---
+++ Assets/Scripts/LetterButtonClicked.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LetterButtonClicked : MonoBehaviour
+{
+    public Button btn;
+    public Text letter;
+    public string txt,all;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        btn.onClick.AddListener(OnBtnClick);
+        txt = GameObject.Find(btn.name).GetComponentInChildren<Text>().text;
+    }
+    public void OnBtnClick()
+    {
+        all += txt;
+        letter.text = all;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
